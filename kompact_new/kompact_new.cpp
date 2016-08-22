99922831046ecb98db451043bc016c64cee6eb3d
AraHaan <seandhunt_7@yahoo.com>
2016-08-22 07:24:13 -0500

    Some thigns on C++ Packer.

--- kompact_new/kompact_new.cpp
+++ kompact_new/kompact_new.cpp
@@ -6,6 +6,20 @@
 
 int main(int argc, char* argv[])
 {
+	//Well for this I could Iterate through all files in a folder that is specified to pack and then.
+	//Force certain files with certain extentions to certain Algorithms that would be known to be specific Algorithms.
+	// TODO: Make a list of Known file Extensions in KOM files and their Algorithms.
+	/*
+		|	File Extension	|	Algorithm	|
+		|	*.txt			|	3			|
+		|	*.tga			|	0			|
+		|	*.lua			|	3			|
+		|	*.dds			|	0			|
+		|	*.x				|	0			|
+		|	*.y				|	0			|
+		|	*.xet			|	0			|
+	*/
+	
     return 0;
 }
 
