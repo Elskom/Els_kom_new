62a55dee25b603cd8b785f65eb2841fa001168f2
AraHaan <seandhunt_7@yahoo.com>
2017-04-14 22:21:41 -0500

    Started on adding an header for the kom file format data structure.
    Hopefully this would help on making an working unpacker.

--- Els_kom_Generic_Unpacker/src/ZManager.hpp
+++ Els_kom_Generic_Unpacker/src/ZManager.hpp
@@ -4,6 +4,10 @@
 
 /*
 	This file is for Managing the use of zlib functions For The Algorithms.
+	This file needs to have methods of doing memory
+	compressions / decompressions without temporary files
+	and return the compressed data crc32's and other things
+	required for the kom format when packing if possible.
 */
 
 //TODO: Figure out how to use all of zlib for this.
