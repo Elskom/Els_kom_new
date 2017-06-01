4c3b7bca8e15c9fb5d4c460d96a5e31818331e3f
AraHaan <seandhunt_7@yahoo.com>
2017-06-01 13:31:40 -0500

    Start working on a zlib class.
    This class is needed touse python's zlib module features without any
    link or dependency to python's core.

---
+++ Els_kom_Generic_Unpacker/src/Zlib.hpp
@@ -0,0 +1,42 @@
+/*
+Header and Implementation to the Zlib class.
+*/
+
+/*
+This is an attempt to pring python's simple to the
+core zlib module to C++. This is because in
+python you can memory compress anything using
+zlib without any manual work on your end to
+set up zlib or anything. As such this is why
+this class is needed to help users use zlib
+like they do in Python with minimal hardships
+trying to figure out why it wont work.
+*/
+
+class Zlib {
+public:
+	/*
+	Compresses the data fed into this
+	function to memory and returns it.
+	
+	Note: It is recommended to feed and assume everything is of the 'unsigned char' or 'unsigned wchar_t' type to be safe than sorry later.
+	
+	Function returns the same data for now until it is fully implemented.
+	*/
+	template<typename T>
+	T compress(T data) {
+		return data;
+	}
+	/*
+	Decompresses the data fed into this
+	function to memory and returns it.
+	
+	Note: It is recommended to feed and assume everything is of the 'unsigned char' or 'unsigned wchar_t' type to be safe than sorry later.
+	
+	Function returns the same data for now until it is fully implemented.
+	*/
+	template<typename T>
+	T decompress(T data) {
+		return data;
+	}
+};
