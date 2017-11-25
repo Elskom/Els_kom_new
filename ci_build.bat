5fd889d92bc0e3bc1e662851b9c059da7b9d062b
AraHaan <seandhunt_7@yahoo.com>
2017-11-25 09:44:23 -0500

    Fixed if check on python.exe.

--- ci_build.bat
+++ ci_build.bat
@@ -3,7 +3,7 @@ call get_externals.bat
 cd externals\cpython
 git pull
 REM build/rebuild python if needed.
-IF EXIST "PCbuild\win32\python.exe" (
+IF EXIST ".\PCbuild\win32\python.exe" (
 "PCbuild\win32\python.exe" "..\..\Python\get_python_build_hash.py"
 ) ELSE (
 call PCBuild\build.bat
