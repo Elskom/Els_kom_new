84901cb25219dd9fab956530c08fe31ce105854c
AraHaan <seandhunt_7@yahoo.com>
2017-11-24 21:26:43 -0500

    Made building python optional in CI.
    Now Python will only build if CI cached build is older than the leatest
    and greatest python 3.6 branch build.

---
+++ Python/get_python_build_hash.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import subprocess
+
+
+def get_git_hash():
+    return sys._git[2]
+
+def get_current_git_hash():
+    proc = subprocess.Popen(
+        'git rev-parse --short HEAD ',
+        stdout=subprocess.PIPE, universal_newlines=True)
+    proc.wait()
+    return proc.stdout
+
+def main():
+    if get_git_hash() == get_current_git_hash():
+        print("Python build is already on latest commit.")
+        return 0
+    else:
+        print("Rebuilding Python...")
+        proc = subprocess.Popen(os.path.join(sys.path[5], 'PCbuild', 'build.bat'), universal_newlines=True)
+        proc.wait()
+
+main()
