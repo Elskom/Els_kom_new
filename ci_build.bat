9ba503d6a87cf0aa1fb2336e8b000af67e2f10ed
AraHaan <seandhunt_7@yahoo.com>
2017-11-24 21:26:43 -0500

    Made building python optional in CI.
    Now Python will only build if CI cached build is older than the leatest
    and greatest python 3.6 branch build.

--- ci_build.bat
+++ ci_build.bat
@@ -1,10 +1,9 @@
+@echo off
 call get_externals.bat
 cd externals\cpython
-REM update cpython extenal if needed.
 git pull
+REM build/rebuild python if needed.
+"PCbuild\win32\python.exe" "..\..\Python\get_python_build_hash.py"
 cd ..\aes
-REM update aes extenal if needed.
 git pull
 cd ..\..
-REM rebuild / build cpython.
-call externals\cpython\PCBuild\build.bat
