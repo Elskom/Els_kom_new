34fe6535c6fcf0f68ed010b1aabb2e491d103724
AraHaan <seandhunt_7@yahoo.com>
2017-11-24 23:05:35 -0500

    Fixed CI.

--- ci_build.bat
+++ ci_build.bat
@@ -3,7 +3,12 @@ call get_externals.bat
 cd externals\cpython
 git pull
 REM build/rebuild python if needed.
+IF EXIST "PCbuild\win32\python.exe" (
 "PCbuild\win32\python.exe" "..\..\Python\get_python_build_hash.py"
+)
+ELSE (
+call PCBuild\build.bat
+)
 cd ..\aes
 git pull
 cd ..\..
