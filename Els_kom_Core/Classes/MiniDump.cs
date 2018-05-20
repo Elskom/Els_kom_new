a4ebbc2bff74b2c8812fb8c4282739e89803daf4
someunknownperson <seandhunt_6@yahoo.com>
2018-05-20 12:49:59 -0400

    Update MiniDump.cs

--- Els_kom_Core/Classes/MiniDump.cs
+++ Els_kom_Core/Classes/MiniDump.cs
@@ -7,25 +7,6 @@ namespace Els_kom_Core.Classes
 {
     public class MiniDump
     {
-        internal enum MINIDUMP_TYPE
-        {
-            MiniDumpNormal = 0x00000000,
-            MiniDumpWithDataSegs = 0x00000001,
-            MiniDumpWithFullMemory = 0x00000002,
-            MiniDumpWithHandleData = 0x00000004,
-            MiniDumpFilterMemory = 0x00000008,
-            MiniDumpScanMemory = 0x00000010,
-            MiniDumpWithUnloadedModules = 0x00000020,
-            MiniDumpWithIndirectlyReferencedMemory = 0x00000040,
-            MiniDumpFilterModulePaths = 0x00000080,
-            MiniDumpWithProcessThreadData = 0x00000100,
-            MiniDumpWithPrivateReadWriteMemory = 0x00000200,
-            MiniDumpWithoutOptionalData = 0x00000400,
-            MiniDumpWithFullMemoryInfo = 0x00000800,
-            MiniDumpWithThreadInfo = 0x00001000,
-            MiniDumpWithCodeSegs = 0x00002000
-        }
-
         public static void MiniDumpToFile(string fileToDump)
         {
             System.IO.FileStream fsToDump = null;
@@ -37,11 +18,12 @@ namespace Els_kom_Core.Classes
             {
                 fsToDump = System.IO.File.Create(fileToDump);
             }
-            Process thisProcess = Process.GetCurrentProcess();
-            MiniDumpWriteDump(thisProcess.Handle, thisProcess.Id,
+            System.Diagnostics.Process thisProcess = System.Diagnostics.Process.GetCurrentProcess();
+            SafeNativeMethods.MiniDumpWriteDump(thisProcess.Handle, thisProcess.Id,
                 fsToDump.SafeFileHandle.DangerousGetHandle(), MINIDUMP_TYPE.MiniDumpNormal,
                 System.IntPtr.Zero, System.IntPtr.Zero, System.IntPtr.Zero);
-            fsToDump.Close();
+            thisProcess.Dispose();
+            fsToDump.Dispose();
         }
     };
 }
