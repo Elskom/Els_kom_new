1550d0e9b915700ab8a472ff6543a9c461e69098
CatGirlsAreLife <20854328+CatGirlsAreLife@users.noreply.github.com>
2018-07-06 15:24:36 -0400

    Start on loading plugins from zip file.

---
+++ Els_kom_Core/Classes/AssemblyExtensions.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2014-2018, Els_kom org.
+// https://github.com/Elskom/
+// All rights reserved.
+// license: MIT, see LICENSE for more details.
+
+namespace Els_kom_Core.Classes
+{
+    internal static class AssemblyExtensions
+    {
+        internal static System.Reflection.Assembly LoadFromZip(string ZipFileName, string AssemblyName)
+        {
+            // check if the assembly is in the zip file.
+            // If it is, get it’s bytes then load it.
+            // If not throw an exception.
+            return null;
+        }
+    }
+}
