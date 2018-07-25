1a4eeb71322d6b9d8be1ad0a81b0020387dbe113
CatGirlsAreLife <20854328+CatGirlsAreLife@users.noreply.github.com>
2018-07-25 04:36:31 -0400

    Create update-newsmake.ps1

---
+++ update-newsmake.ps1
@@ -0,0 +1,46 @@
+Set-Location -Path externals
+Set-Location -Path newsmake
+try
+{
+    $env:NEWSMAKE_REPO_HEAD = Join-Path (Get-Location) .git/HEAD
+    $stream = [System.IO.StreamReader]::new($env:NEWSMAKE_REPO_HEAD)
+    $env:NEWSMAKE_CURRENT_COMMIT_ID = $stream.ReadLine()
+}
+finally
+{
+    $stream.close()
+}
+Set-Location -Path ..
+$env:nsmkpth = Join-Path (Get-Location) newsmake
+if(!(Test-Path -Path $env:nsmkpth))
+{
+    git clone -q https://github.com/Elskom/newsmake.git
+    Set-Location -Path newsmake/build
+    cmake ..
+    Set-Location -Path ../..
+}
+else
+{
+    Set-Location -Path newsmake
+    git pull -q
+    Set-Location -Path ..
+}
+Set-Location -Path newsmake/build
+try
+{
+    $stream = [System.IO.StreamReader]::new($env:NEWSMAKE_REPO_HEAD)
+    $env:NEWSMAKE_NEW_COMMIT_ID = $stream.ReadLine()
+}
+finally
+{
+    $stream.close()
+}
+if (!($env:NEWSMAKE_NEW_COMMIT_ID -eq $env:NEWSMAKE_CURRENT_COMMIT_ID))
+{
+    msbuild newsmake.sln /p:Configuration=Release /p:Platform="Win32" /nologo /verbosity:m /m
+}
+Set-Location -Path ../../
+git clone -q https://github.com/Elskom/ZLIB.NET.git --branch patches
+Set-Location -Path ../Misc/NEWS
+"../../externals/newsmake/build/Release/newsmake"
+Set-Location -Path ../..
