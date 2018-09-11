aba60e449007f800b1b94f10afdf90b7c2b97be7
CatGirlsAreLife <20854328+CatGirlsAreLife@users.noreply.github.com>
2018-09-11 00:38:57 -0400

    Do not clone plugins repository.

--- package-release.cmd
+++ package-release.cmd
@@ -1,9 +1,5 @@
 @echo off
 
-cd Plugins
-git clone -q https://github.com/Elskom/Els_kom_plugins.git
-cd ..
-msbuild Plugins\Els_kom_plugins\DefaultPlugins.sln /m /verbosity:minimal
 cd "bin/%CONFIGURATION%/"
 "Els_kom" -p "./Els_kom_new.%CONFIGURATION%.%APPVEYOR_BUILD_WORKER_IMAGE%.zip"
 cd ../..
