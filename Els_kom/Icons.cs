09ca7c655868cdba309620054168bfee21f905e7
AraHaan <seandhunt_7@yahoo.com>
2024-02-19 04:43:02 -0500

    Update year to 2024.

--- Els_kom/Icons.cs
+++ Els_kom/Icons.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2023, Els_kom org.
+// Copyright (c) 2014-2024, Els_kom org.
 // https://github.com/Elskom/
 // All rights reserved.
 // license: MIT, see LICENSE for more details.
